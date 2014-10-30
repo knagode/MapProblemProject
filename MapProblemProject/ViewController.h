bbecbd13576d285456f9a3f89be2d581e51d99d2
Klemen Nagode <klemen.nagode@gmail.com>
2014-10-30 12:05:16 +0100

    init

--- MapProblemProject/ViewController.h
+++ MapProblemProject/ViewController.h
@@ -1,14 +1,15 @@
 //
 //  ViewController.h
-//  MapProblemProject
+//  xxxx
 //
 //  Created by Klemen Nagode on 30/10/14.
 //  Copyright (c) 2014 Klemen Nagode. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <MKMapViewDelegate>
 
 
 @end
